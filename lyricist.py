31247f9a52a9fb65900874877a10be7e3eb4f096
Mango <KenNewcombNJ@gmail.com>
2014-05-26 18:16:56 -0400

    Refactoring: Better separation of functions.

--- lyricist.py
+++ lyricist.py
@@ -5,67 +5,14 @@ import urllib
 import re
 import time
 import sys
-
-def nopunc(s):
-    return ''.join(e for e in s if e.isalnum())
-
-def getSong():
-	"""Grabs the currently playing song from Banshee"""
-	Artist = os.popen('banshee --query-artist').readline().replace('artist: ', '').replace('\n', '')
-	Title = os.popen('banshee --query-title').readline().replace('title: ', '').replace('\n', '')
-	song = [Artist,Title]
-	return song
-
-def getLyrics(song):
-	"""Fetches the lyrics from the internet"""
-	# Prepare Artist and Title for lyrics query.
-	Artist = song[0]
-	Title = song[1]
-	artist = nopunc(Artist.replace('The', '')).lower()
-	title = nopunc(Title).lower()
+import fetchLyrics, nowPlaying
 	
-	AZLyrics(artist, title)
-
-def AZLyrics(artist, title):
-	# construct the lyrics URL
-	urlstring = "http://www.azlyrics.com/lyrics/{0}/{1}.html".format(artist, title)
-	# get the HTML at the proper URL
-	lyrics = urllib.urlopen(urlstring)
-
-	try:
-		raw_lyrics = lyrics.read().split('<!-- start of lyrics -->')[1]
-	except IndexError:
-		print "AZLyrics: No lyrics found..."
-		return false
-	
-	raw_lyrics = raw_lyrics.split('<!-- end of lyrics -->')[0]
-	
-	processed_lyrics = cleanLyrics(raw_lyrics) 
-
-	print processed_lyrics
-	return True
-
-
-def cleanLyrics(lyrics):
-	"""Cleans up lyrics, removing HTML tags"""
-	# remove HTML tags
-	lyrics = re.sub('<.*?>', '', lyrics)
-	return lyrics
-	
-	
-	title = "{0} by {1}".format(Title, Artist)
-	print title
-	
-	for characters in title:
-		sys.stdout.write('-')		
-
 # Program Start:
-song = getSong()
-getLyrics(song)
+song = nowPlaying.getSong()
+fetchLyrics.getLyrics(song)
 
 while True:
-	if song != getSong():
-		song = getSong() 
-		raw_lyrics = getLyrics(song)
-		processed_lyrics = cleanLyrics(raw_lyrics)
+	if song != nowPlaying.getSong():
+		song = nowPlaying.getSong() 
+		fetchLyrics.getLyrics(song)
 	time.sleep(1)
