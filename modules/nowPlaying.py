6efb725dace3fc3fedd13f5f3ad6c2f1eacb696c
Ken Newcomb <knewco02@villanova.edu>
2014-09-22 15:46:24 -0400

    Initial Python 3 functionality

--- modules/nowPlaying.py
+++ modules/nowPlaying.py
@@ -1,12 +1,20 @@
 import os
-import commands
+import subprocess
 import dbus
 import re
 import sys
 
+def getPID():
+	"""Determines if a supported music player is currently running"""
+	processes = subprocess.check_output('ps','-A')
+	software = ['banshee', 'rhythmbox']
+	if software in processes:
+		getSong(software)
+	else:
+		return False
 def getSong():
 	"""Grabs the currently playing song from Banshee"""
-	Artist = os.popen('banshee --query-artist').readline().replace('artist: ', '').replace('\n', '')
-	Title = os.popen('banshee --query-title').readline().replace('title: ', '').replace('\n', '')
+	Artist = subprocess.check_output('banshee --query-artist', shell=True).decode().replace('artist: ', '').replace('\n', '')
+	Title = subprocess.check_output('banshee --query-title', shell=True).decode().replace('title: ', '').replace('\n', '')
 	song = [Artist,Title]
 	return song
